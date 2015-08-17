a912a55dd6f76d1a80f92ad821759e9eb4312f27
Howard Yeh <howard@metacircus.com>
2015-08-17 15:57:34 +0800

    ScrollView jitter demo

--- index.ios.js
+++ index.ios.js
@@ -10,43 +10,79 @@ var {
   StyleSheet,
   Text,
   View,
+  Image,
+
+  TouchableOpacity,
+
+  ScrollView,
 } = React;
 
+var REFRESH_HEADER_HEIGHT = 50;
+
 var ScrollViewContentInsetChangeJitter = React.createClass({
+  getInitialState: function() {
+    return {
+      topInset: -REFRESH_HEADER_HEIGHT,
+    };
+  },
+
+  handleScroll: function(e) {
+    var {contentInset,contentOffset} = e.nativeEvent;
+    console.log("offset y",contentOffset.y);
+    if(contentOffset.y < 0 && this.state.topInset != 0) {
+      this.setState({topInset: 0});
+    }
+  },
+
+  hideHeader: function() {
+    console.log("hide header");
+    this.setState({topInset: -REFRESH_HEADER_HEIGHT});
+  },
+
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <ScrollView style={styles.scrollView}
+        contentOffset={{y: -this.state.topInset}} /* reset initial offset */
+        contentInset={{top: this.state.topInset}}
+        onScroll={this.handleScroll}
+        scrollEventThrottle={4}
+        automaticallyAdjustContentInsets={false}>
+        <Image style={styles.image} source={require("image!hikers")}/>
+
+        <View style={styles.refreshHeader}>
+          <TouchableOpacity onPress={this.hideHeader}>
+            <Text>Tap to hide refresh header</Text>
+          </TouchableOpacity>
+        </View>
+
+      </ScrollView>
     );
   }
 });
 
 var styles = StyleSheet.create({
-  container: {
+  scrollView: {
     flex: 1,
+
+  },
+
+  refreshHeader: {
+    backgroundColor: 'rgba(255,0,0,0.3)',
+
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 50,
+
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  image: {
+    flex: 1,
+    // width: 400,
+    height: 800,
   },
 });
 
